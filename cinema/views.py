9381d1eaffff38bdb7f608e0810dedc0e88b2163
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-08-07 08:23:01 +0300

    update view.py is_valid

--- cinema/views.py
+++ cinema/views.py
@@ -28,7 +28,7 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
